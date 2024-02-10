6b4dcaf801f6647855588cda9641756c393f31fc
RusulAzomSumon <18103020@iubat.edu>
2024-02-10 21:19:06 +0600

    funtion P

---
+++ basic/main.js
@@ -0,0 +1,37 @@
+// genaral function 
+// let x = 5;
+// let y = 4;
+// function sum(a, b){
+//     return a+b;
+// }
+// console.log(sum(x,y));
+
+// 1. arrow function 
+
+// const addVal = (a,b) => a-b;
+
+// console.log(addVal(3,"4"));
+
+// const hi = () => console.log("Hi");
+// hi();
+
+// **************2. Anonymous function**************
+// const buttonClickHandler = function() {
+//     console.log("Button clicked!");
+//   };
+  
+//   button.addEventListener("click", buttonClickHandler);
+
+// **************3. Recursive function:**************
+
+// function factorial(n){
+//     if(n===0){
+//         return 1;
+//     }
+//     else{
+//         return n*factorial(n-1);
+//     }
+        
+// }
+
+// console.log(factorial(5));
